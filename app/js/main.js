8b57eaf3565eeb23a952668650d9f0860981d4c8
Ryan Flaherty <rcflaherty@gmail.com>
2015-06-30 15:10:30 -0700

    catalog input ids and names updated, jquery added for cart buttons

--- app/js/main.js
+++ app/js/main.js
@@ -17,4 +17,56 @@ $('.stored').change(function () {
 });
 
 // cart
+var cart = [];
+
+var cartItem = function(style, size, qty) {
+  this.style = style;
+  this.price = 32;
+  this.size = size;
+  this.qty = qty;
+};
+
+$('#aztec_mint_cart').on('click', function(e){
+  e.preventDefault();
+  var style = 'aztec';
+  var size = $('#aztec_mint_size').val();
+  var qty = $('#aztec_mint_qty').val();
+  var order = new cartItem(style, size, qty);
+  cart.push(order);
+});
+
+$('#the_basics_cart').on('click', function(e){
+  e.preventDefault();
+  var style = 'basics';
+  var size = $('#the_basics_size').val();
+  var qty = $('#the_basics_qty').val();
+  var order = new cartItem(style, size, qty);
+  cart.push(order);
+});
+
+$('#purple_mountain_cart').on('click', function(e){
+  e.preventDefault();
+  var style = 'purple';
+  var size = $('#purple_mountain_size').val();
+  var qty = $('#purple_mountain_qty').val();
+  var order = new cartItem(style, size, qty);
+  cart.push(order);
+});
+
+$('#wknd_cmdr_cart').on('click', function(e){
+  e.preventDefault();
+  var style = 'wknd_cmdr';
+  var size = $('#wknd_cmdr_size').val();
+  var qty = $('#wknd_cmdr_qty').val();
+  var order = new cartItem(style, size, qty);
+  cart.push(order);
+});
+
+$('#log').on('click', function() {
+  cart.filter(function(cartItem){
+    $.each(cartItem, function(){
+      console.log(cartItem.style + ", " + cartItem.size + ", " + cartItem.qty);
+    });
+  });
+});
 
