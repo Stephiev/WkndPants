8f2b547ccb5fd7537c31db25b16b8083e26e84b5
Ryan Flaherty <rcflaherty@gmail.com>
2015-06-30 16:24:35 -0700

    pants to cart UI complete, local storage next

--- app/js/main.js
+++ app/js/main.js
@@ -17,56 +17,64 @@ $('.stored').change(function () {
 });
 
 // cart
-var cart = [];
+var cart = [ ];
 
-var cartItem = function(style, size, qty) {
-  this.style = style;
-  this.price = 32;
-  this.size = size;
-  this.qty = qty;
+var shoppingCart = [
+  {"style": "The Basics", "quantity": [0, 0, 0]},
+  {"style": "Purple Mountain", "quantity": [0, 0, 0]},
+  {"style": "WKND Commander", "quantity": [0, 0, 0]},
+  {"style": "Aztec Mint", "quantity": [0, 0, 0]}
+];
+
+var parseSize = function(size) {
+  if (size == 'size_S') {
+    return 0;
+  } else if (size == 'size_M') {
+    return 1;
+  } else if (size == 'size_L') {
+    return 2;
+  };
 };
 
 $('#aztec_mint_cart').on('click', function(e){
   e.preventDefault();
-  var style = 'aztec';
   var size = $('#aztec_mint_size').val();
   var qty = $('#aztec_mint_qty').val();
-  var order = new cartItem(style, size, qty);
-  cart.push(order);
+  var sizeVal = parseSize(size);
+  shoppingCart[3].quantity[sizeVal] += eval(qty);
+  console.log(shoppingCart[3].style + ", " + shoppingCart[3].quantity);
 });
 
 $('#the_basics_cart').on('click', function(e){
   e.preventDefault();
-  var style = 'basics';
   var size = $('#the_basics_size').val();
   var qty = $('#the_basics_qty').val();
-  var order = new cartItem(style, size, qty);
-  cart.push(order);
+  var sizeVal = parseSize(size);
+  shoppingCart[0].quantity[sizeVal] += eval(qty);
+  console.log(shoppingCart[0].style + ", " + shoppingCart[0].quantity);
 });
 
 $('#purple_mountain_cart').on('click', function(e){
   e.preventDefault();
-  var style = 'purple';
   var size = $('#purple_mountain_size').val();
   var qty = $('#purple_mountain_qty').val();
-  var order = new cartItem(style, size, qty);
-  cart.push(order);
+  var sizeVal = parseSize(size);
+  shoppingCart[1].quantity[sizeVal] += eval(qty);
+  console.log(shoppingCart[1].style + ", " + shoppingCart[1].quantity);
 });
 
 $('#wknd_cmdr_cart').on('click', function(e){
   e.preventDefault();
-  var style = 'wknd_cmdr';
   var size = $('#wknd_cmdr_size').val();
   var qty = $('#wknd_cmdr_qty').val();
-  var order = new cartItem(style, size, qty);
-  cart.push(order);
+  var sizeVal = parseSize(size);
+  shoppingCart[2].quantity[sizeVal] += eval(qty);
+  console.log(shoppingCart[2].style + ", " + shoppingCart[2].quantity);
 });
 
 $('#log').on('click', function() {
-  cart.filter(function(cartItem){
-    $.each(cartItem, function(){
-      console.log(cartItem.style + ", " + cartItem.size + ", " + cartItem.qty);
-    });
-  });
+  console.log(shoppingCart[0].style + ", " + shoppingCart[0].quantity);
+  console.log(shoppingCart[1].style + ", " + shoppingCart[1].quantity);
+  console.log(shoppingCart[2].style + ", " + shoppingCart[2].quantity);
+  console.log(shoppingCart[3].style + ", " + shoppingCart[3].quantity);
 });
-
