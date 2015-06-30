2b8f37d0c85cc125c1d5cf15402cba959d54e277
Josh Zylstra <zylstrajosh@gmail.com>
2015-06-30 07:41:36 -0700

    orderpage mvp

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,19 @@
 
 // placeholder
+//retrieves localStorage data for form fields
+$(document).ready(function () {
+  function init() {
+    $('.stored').each(function(){
+      var name = $(this).attr('name');
+      var val = $(this).val();
+      if (localStorage[name]) {
+        $(this).val(localStorage[name]);
+      }
+    });
+  }
+  init();
+});
+//stores form data on keyup with class of "stored"
+$('.stored').keyup(function () {
+  localStorage[$(this).attr('name')] = $(this).val();
+});
