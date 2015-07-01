722d7588a7aac5824745d8c7ffcc3c5a5b44f1d4
Ryan Flaherty <rcflaherty@gmail.com>
2015-07-01 14:33:35 -0700

    pants localstorage fixed

--- app/js/main.js
+++ app/js/main.js
@@ -51,51 +51,50 @@ var shoppingCart = [
     }
   }
 ];
-var basic = shoppingCart[0];
-var purple = shoppingCart[1];
-var wknd = shoppingCart[2];
-var aztec = shoppingCart[3];
-
 
 $('#aztec_mint_cart').on('click', function(e){
   e.preventDefault();
+  var aztec = shoppingCart[3];
   var pantSize = $('#aztec_mint_size').val();
   var qty = $('#aztec_mint_qty').val();
   aztec.sizes[pantSize] += eval(qty);
   storeCart();
-  console.log(aztec.style + ", " + pantSize + ", " + aztec.sizes[pantSize]);
 });
 
 $('#the_basics_cart').on('click', function(e){
   e.preventDefault();
+  var basics = shoppingCart[0];
   var pantSize = $('#the_basics_size').val();
   var qty = $('#the_basics_qty').val();
-  basic.sizes[pantSize] += eval(qty);
+  basics.sizes[pantSize] += eval(qty);
   storeCart();
-  console.log(basic.style + ", " + pantSize + ", " + basic.sizes[pantSize]);
 });
 
 $('#purple_mountain_cart').on('click', function(e){
   e.preventDefault();
+  var purple = shoppingCart[1];
   var pantSize = $('#purple_mountain_size').val();
   var qty = $('#purple_mountain_qty').val();
   purple.sizes[pantSize] += eval(qty);
   storeCart();
-  console.log(purple.style + ", " + pantSize + ", " + purple.sizes[pantSize]);
 });
 
 $('#wknd_cmdr_cart').on('click', function(e){
   e.preventDefault();
+  var wknd = shoppingCart[2];
   var pantSize = $('#wknd_cmdr_size').val();
   var qty = $('#wknd_cmdr_qty').val();
   wknd.sizes[pantSize] += eval(qty);
   storeCart();
-  console.log(wknd.style + ", " + pantSize + ", " + wknd.sizes[pantSize]);
 });
 
 $('#log').on('click', function() {
+  var basics = shoppingCart[0];
+  var purple = shoppingCart[1];
+  var wknd = shoppingCart[2];
+  var aztec = shoppingCart[3];
   console.log(aztec.style + ", S:" + aztec.sizes.small + ", M:" + aztec.sizes.medium + ", L:" + aztec.sizes.large);
-  console.log(basic.style + ", S:" + basic.sizes.small + ", M:" + basic.sizes.medium + ", L:" + basic.sizes.large);
+  console.log(basics.style + ", S:" + basics.sizes.small + ", M:" + basics.sizes.medium + ", L:" + basics.sizes.large);
   console.log(purple.style + ", S:" + purple.sizes.small + ", M:" + purple.sizes.medium + ", L:" + purple.sizes.large);
   console.log(wknd.style + ", S:" + wknd.sizes.small + ", M:" + wknd.sizes.medium + ", L:" + wknd.sizes.large);
 });
@@ -108,8 +107,7 @@ var storeCart = function() {
 };
 
 $(document).ready(function(){
-  var ifCartStored = JSON.parse(localStorage.getItem('cartStored'));
-  if (ifCartStored == true) {
+  if (JSON.parse(localStorage.getItem('cartStored')) == true) {
     var retrieveCart = JSON.parse(localStorage.getItem('shoppingCart'));
       shoppingCart = retrieveCart;
     };
