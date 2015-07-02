a53b19689b734d3686b9fd2054db3ef58791262e
Ryan Flaherty <rcflaherty@gmail.com>
2015-07-01 22:59:48 -0700

    changed DOM traversal for cart.js so spans can be added

--- app/js/cart.js
+++ app/js/cart.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     function carting(shoppingCart, sizeRequested) {
       for (var i = 0; i < 4; i++) {
         if (shoppingCart[i].sizes[sizeRequested.toLowerCase()] > 0) {
-          $('.cart_heading').after('<li>'+ shoppingCart[i].style + ', ' + 'Size: <span class="this_size">' + sizeRequested + '</span>,' + ' ' + 'Price: $32,' + ' qty:' + '<input type="number" class="cart_qty_input" min="1" max="10" value="' + shoppingCart[i].sizes[sizeRequested.toLowerCase()] + '"></input><button class="delete" value="' + i + '">X</button><li>');
+          $('.cart_heading').after('<li id="' + sizeRequested + i + '">'+ shoppingCart[i].style + ', ' + 'Size: <span class="this_size">' + sizeRequested + '</span>,' + ' ' + 'Price: $32,' + ' qty:' + '<input type="number" class="cart_qty_input" min="1" max="10" value="' + shoppingCart[i].sizes[sizeRequested.toLowerCase()] + '"></input><button type="button" name="' + sizeRequested + '" class="delete" value="' + i + '">X</button><li>');
         }
       }
     }
@@ -33,16 +33,17 @@ $(document).ready(function(){
 
   $('.delete').on('click', function(){
     var voidItem = $(this).val();
-    var voidSize = $(this).siblings('.this_size').text();
+    var voidSize = $(this).attr('name');
+    var listItemID = '#' + voidSize + voidItem;
     shoppingCart[voidItem].sizes[voidSize.toLowerCase()] = 0;
     storeCart();
-    $(this).parent().detach();
+    $(listItemID).detach();
     subTotal();
   })
 
   $('.cart_qty_input').change(function(){
     var changeItem = $(this).next().val();
-    var changeSize = $(this).siblings('.this_size').text();
+    var changeSize = $(this).next().attr('name');
     shoppingCart[changeItem].sizes[changeSize.toLowerCase()] = $(this).val();
     storeCart();
     subTotal();
