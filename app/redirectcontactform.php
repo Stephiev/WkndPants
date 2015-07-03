9c505f2825b5a59f0f1bef73fa092ad5abd24c8b
Josh Zylstra <zylstrajosh@gmail.com>
2015-07-03 00:28:32 -0700

    Working Contact Page

---
+++ app/redirectcontactform.php
@@ -0,0 +1,62 @@
+<?php
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>WKND Pants | Cart</title>
+    <meta property="og:site_name" content="Weekend Pants Unlimited"/>
+    <meta property="og:description" content="The Weekend is Calling"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/modules.css">
+    <link rel="stylesheet" href="css/states.css">
+  </head>
+  <body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <header>
+      <img class="wknd_logo" src="images/wkndpantslogo.png" />
+      <h1 class="logo_text">Weekend Pants Unlimited</h1>
+      <nav>
+        <ol>
+          <li class="nav_links">
+            <a href="index.html">Home</a>
+          </li>
+          <li class="nav_links">
+            <a href="pants.html">Pants</a>
+          </li>
+          <li class="nav_links">
+            <a href="about.html">About</a>
+          </li>
+          <li class="nav_links">
+            <a href="">Cart</a>
+          </li>
+        </ol>
+      </nav>
+    </header>
+    <section>
+      <p style="text-align: center; font-size: 1.2em; font-weight: 200; margin-top: 4em;">Thanks For Reaching Out!<br><br>We will get back to you as soon as possible!</p>
+      <h1 style="text-align: center; font-size: 4em; font-weight: 200; margin: 1em auto 2em auto;"><a href="pants.html">SHOP PANTS NOW</a></h1>
+    </section>
+    <footer>
+      <div class="footer_social_icons">
+        <a href="https://twitter.com/">
+          <span class="icon-twitter"></span>
+        </a>
+      </div>
+      <h3><strong>Seattle </strong> ♥ <strong> Detroit</strong></h3>
+      <p class="footer_text">Copyright &copy 2014 Weekend Pants Unlimited. All Rights Reserved.</p>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/cart.js"></script>
+  </body>
+<?php
+
+?>
